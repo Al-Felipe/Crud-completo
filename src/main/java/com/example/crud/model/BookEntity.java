456e1898fdceeb598129dcad25db712e5eb58618
Felipe Alves <felipealves3580@gmail.com>
2024-08-16 10:57:22 -0300

    create book entity

---
+++ src/main/java/com/example/crud/model/BookEntity.java
@@ -0,0 +1,50 @@
+package com.example.crud.model;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//especifica o nome da colecao do mongo que vai ser usada
+@Document("books")
+public class BookEntity {
+    @Id
+    private String id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String author;
+    @NotBlank
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
