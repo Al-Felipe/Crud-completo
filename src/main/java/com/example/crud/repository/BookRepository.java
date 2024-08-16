9d6a0cf9ed3cc1f610f7fb7267519f71d9b0957d
Felipe Alves <felipealves3580@gmail.com>
2024-08-16 18:07:52 -0300

    create findById via String id

--- src/main/java/com/example/crud/repository/BookRepository.java
+++ src/main/java/com/example/crud/repository/BookRepository.java
@@ -4,6 +4,9 @@ import com.example.crud.model.BookEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends MongoRepository<BookEntity, Long> {
+    Optional<BookEntity> findById(String id);
 }
