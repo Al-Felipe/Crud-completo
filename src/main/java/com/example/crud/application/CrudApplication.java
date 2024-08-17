5e0d7fcc05cc5a810fbfb02aaaeda8f986850782
Felipe Alves <felipealves3580@gmail.com>
2024-08-17 09:13:39 -0300

    enable mongo repositorie

--- src/main/java/com/example/crud/application/CrudApplication.java
+++ src/main/java/com/example/crud/application/CrudApplication.java
@@ -2,8 +2,10 @@ package com.example.crud.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
+@EnableMongoRepositories("com.example.crud.repository")
 public class CrudApplication {
 
 	public static void main(String[] args) {
